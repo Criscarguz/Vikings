439c12779e3b0b09d37a806fb2e12f9d7f3008f1
criscarguz <criscarguz@gmail.com>
2017-07-18 09:37:27 +0200

    correcciones

--- Vikingos.js
+++ Vikingos.js
@@ -5,20 +5,21 @@ var Compradoras = function(name, health, strength){
 	this.health = health;
 	this.strength = strength;
 };
-var names = ['Fernando','Fernandito','Fernandinho','Reyes de Oriente','Gregorio','Aurorita','Aurora','Almu','Eli','Pilarita',
+
+var random = function (min, max){
+	return Math.round(Math.random() * (max - min) + min);
+};
+function randomName(){
+	var names = ['Fernando','Fernandito','Fernandinho','Reyes de Oriente','Gregorio','Aurorita','Aurora','Almu','Eli','Pilarita',
 			'Almudena','Reyes','Elisabet','Pilar','Javi','Cesar','Santi','Javier','Julio Cesar', 'Santiago', 'Gabriel',
 			'Alvarito', 'Juan Carlos','Gabri','Alvaro','Juanca','Guille','Andres','Aaron','Miguel','Cris', 'Guillermo',
 			'Andresito','Mesurado', 'Vicaria', 'Cristian'];
-var randomName = Math.floor(Math.random()*(names.length));
-var selectName = names[randomName];
+ return names[random(0, names.length)];
+}
 
-var totalHealth = function (min, max){
-	return Math.round(Math.random() * (max - min) + min);
-};
-var strength = function (min, max){
-	return Math.round(Math.random() * (max - min) + min);
-};
-//var compradora1 = new Compradoras(selectName,totalHealth (20,100), strength(5, 15));
+
+
+//var compradora1 = new Compradoras(randomName(),totalHealth (20,100), strength(5, 15));
 
 
 //Dinosaurios
@@ -28,23 +29,18 @@ var Dinosaurios = function (health, strength){
 	this.health = health;
 	this.strength = strength;
 };
- var dinosaurio1 = new Dinosaurios(totalHealth(5, 25), strength(1,8));
+ var dinosaurio1 = new Dinosaurios(random(5, 25), random(1,8));
 
 //Entrenamiento
 
-var compradora1 = new Compradoras(selectName,totalHealth (20,100), strength(5, 15));
-var compradora2 = new Compradoras(selectName,totalHealth (20,100), strength(5, 15));
+var compradora1 = new Compradoras(randomName(),random(20,100), random(5, 15));
+var compradora2 = new Compradoras(randomName(),random(20,100), random(5, 15));
 
 function training (compradora1, compradora2) {
 	while (compradora1.health >=15 && compradora2.health >= 15){
-		 var start = Math.floor(Math.random())+1;
-		 if (start == 1){
-			 compradora2.health = compradora2.health - compradora1.strength;
-			 compradora1.health = compradora1.health - compradora2.strength;
-			} else{
-			 compradora1.health = compradora1.health - compradora2.strength;
-			 compradora2.health = compradora2.health - compradora1.strength;
-			}
+		compradora2.health = compradora2.health - compradora1.strength;
+		compradora1.health = compradora1.health - compradora2.strength;
+			
   console.log(compradora2.health, compradora1.health);
 	} 	
 	if (compradora1.health > compradora2.health){
@@ -52,7 +48,6 @@ function training (compradora1, compradora2) {
 	} else { console.log (compradora2.name + ' ha resultado victoriosa, gana los zapatos!!');
 	  
 	}
-
 }
 training(compradora1,compradora2);
 
